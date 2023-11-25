c1ea778938afd615a8a83c4082af74c083113ce7
Vojtech Bocek <vbocek@gmail.com>
2023-11-25 09:59:22 +0100

    feat: add captive portal info message

--- web/js/99_footer.js
+++ web/js/99_footer.js
@@ -1,4 +1,15 @@
 window.addEventListener('load', function () {
+  // Detect captive portal and show message
+  var ua = window.navigator.userAgent;
+  if(ua.indexOf("; wv)") !== -1 && ua.indexOf("Chrome/") !== -1 && ua.indexOf("Version/") !== -1 && ua.indexOf("Android") !== -1) {
+    document.getElementById("main-ui").style.display = "none";
+    document.getElementById("captive-msg").style.display = "block";
+
+    document.getElementById("captive-url").innerText = window.location.origin;
+    document.getElementById("captive-link").href = "intent://" + window.location.hostname + "#Intent;scheme=http;end";
+    return;
+  }
+
   var man = new Manager('log', 'grid')
 
   // Detect whether we're running in RBController or just web browser
@@ -8,3 +19,8 @@ window.addEventListener('load', function () {
     man.start('ws://' + window.location.host, true)
   }
 })
+
+function closeCaptivePortalNotice() {
+  document.getElementById('main-ui').style.display = "block";
+  document.getElementById('captive-msg').style.display = 'none'
+}
