f55a28a2a8d8fc485ac369b859fd32621eb7dd21
Tasssadar <Tasssadar@users.noreply.github.com>
2024-07-02 20:24:27 +0000

    deploy: 8790a10379ca1892f80860839cf8abc8f3fe155f

--- search/all_8.js
+++ search/all_8.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['handlerbpacket_0',['handleRbPacket',['../classgridui_1_1__GridUi.html#ad734093c796089ef7cdd9aa13f1bb5b4',1,'gridui::_GridUi']]],
   ['handlers_1',['Event handlers',['../group__event.html',1,'']]],
-  ['hash_5fcount_2',['hash_count',['../namespacegridui.html#ad82f23f20ca9eca46ca41845c962d341',1,'gridui']]]
+  ['hash_5fcount_2',['hash_count',['../namespacegridui.html#ad82f23f20ca9eca46ca41845c962d341',1,'gridui']]],
+  ['hasregisteredcallbacks_3',['hasregisteredcallbacks',['../classgridui_1_1WidgetState.html#a773b556850d89e54b3cff5e741aaf62b',1,'gridui::WidgetState::hasRegisteredCallbacks()'],['../classgridui_1_1Widget.html#a2955c3a26a44cf2ae7eb44e3cd014d86',1,'gridui::Widget::hasRegisteredCallbacks()']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['handlerbpacket_0',['handleRbPacket',['../classgridui_1_1__GridUi.html#ad734093c796089ef7cdd9aa13f1bb5b4',1,'gridui::_GridUi']]]
+  ['handlerbpacket_0',['handleRbPacket',['../classgridui_1_1__GridUi.html#ad734093c796089ef7cdd9aa13f1bb5b4',1,'gridui::_GridUi']]],
+  ['hasregisteredcallbacks_1',['hasregisteredcallbacks',['../classgridui_1_1WidgetState.html#a773b556850d89e54b3cff5e741aaf62b',1,'gridui::WidgetState::hasRegisteredCallbacks()'],['../classgridui_1_1Widget.html#a2955c3a26a44cf2ae7eb44e3cd014d86',1,'gridui::Widget::hasRegisteredCallbacks()']]]
 ];
